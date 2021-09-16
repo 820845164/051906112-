a24b01176f0628f57161c6a1a9bee7710fd994fa
820845164 <90021033+820845164@users.noreply.github.com>
2021-09-16 15:58:17 +0800

    修改了下

--- 051906112/main.py
+++ 051906112/main.py
@@ -103,6 +103,30 @@ def main_test():
                 f.write(i)
             f.close()
 
+def test(path_1, path_2):
+    if not os.path.exists(path_1):
+        print("文件不存在")
+        exit()
+    if not os.path.exists(path_2):
+        print("文件不存在")
+        exit()
+    words = get_sensitivewords(path_1)
+    contents = get_sensitivecontent(path_2)
+    re_1 = []
+    for i in words:
+        re_1.append(forming_re(i))
+    # 生成敏感词的所有re表达式
+    answer = []
+    for index in range(len(contents)):
+        # 开始循环print()所有的敏感词
+        line = contents[index]
+        index = index + 1
+        for j in range(len(words)):
+            re_2 = re_1[j]
+            for p in re.findall(re_2, line, re.IGNORECASE):
+                answer.append(f"Line{index}: <{words[j]}> {p}")
+    print(answer)
+    return answer
 
 if __name__ == '__main__':
     main_test()
